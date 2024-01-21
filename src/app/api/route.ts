d22ada38c47e4b1189af48b274590d7ad97718ba
Jordan Molina <jordanmichaelmolina3@gmail.com>
2024-01-21 11:30:42 -0600

    cache fetch data

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,10 +1,42 @@
-export async function GET(request: Request) {
-  const res = Response.json(
-    { message: "Hello World!" },
-    {
-      status: 200,
-    }
-  );
+import { ApiError } from "@the-convocation/twitter-scraper/dist/errors";
+import User from "../../app/_types/User";
+import { Profile, Scraper, Tweet } from "@the-convocation/twitter-scraper";
 
-  return res;
+export async function GET(request: Request) {
+  const scraper = new Scraper();
+
+  let userData: Profile;
+  let tweetsData: Tweet[] = [];
+
+  try {
+    return scraper
+      .getProfile("zherkaofficial")
+      .then((user) => {
+        userData = user;
+        if (user.userId) return scraper.getTweetsByUserId(user.userId, 5);
+      })
+      .then(async (tweets) => {
+        if (tweets) {
+          for await (const tweet of tweets) {
+            if (tweet) tweetsData.push(tweet);
+          }
+        }
+
+        const user: User = {
+          user: userData,
+          tweets: tweetsData,
+        };
+        return user;
+      })
+      .then((user) => {
+        return Response.json(
+          { data: user },
+          {
+            status: 200,
+          }
+        );
+      });
+  } catch (error) {
+    throw ApiError;
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,65 +2,21 @@ import TweetWrapper from "../components/Tweets/TweetWrapper";
 import Matrix from "../components/Matrix/Matrix";
 import { Profile, Scraper, Tweet } from "@the-convocation/twitter-scraper";
 import User from "../app/_types/User";
-import { ApiError } from "@the-convocation/twitter-scraper/dist/errors";
-import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
 
 async function getTwitterProfile() {
-  const scraper = new Scraper();
-
-  let userData: Profile;
-  let tweetsData: Tweet[] = [];
-  const test = await fetch("http://localhost:3000/api", {
+  const userResponse = await fetch("http://localhost:3000/api", {
     next: { revalidate: 3600 },
   });
 
-  console.log(await test.json());
-
-  // try {
-  //   return scraper
-  //     .getProfile("zherkaofficial")
-  //     .then((user) => {
-  //       userData = user;
-  //       if (user.userId) return scraper.getTweetsByUserId(user.userId, 5);
-  //     })
-  //     .then(async (tweets) => {
-  //       if (tweets) {
-  //         for await (const tweet of tweets) {
-  //           if (tweet) tweetsData.push(tweet);
-  //         }
-  //       }
-
-  //       const final: User = {
-  //         user: userData,
-  //         tweets: tweetsData,
-  //       };
-
-  //       return final;
-  //     })
-  //     .then((data) => {
-  //       //console.log(data);
-  //       return data;
-  //     });
-  // } catch (error) {
-  //   throw ApiError;
-  // }
-}
-
-async function getData() {
-  const res = await fetch(
-    "https://newsapi.org/v2/top-headlines?country=us&apiKey=1871f253a7524cce8c65a68960f516b5&pageSize=3&page=3",
-    { next: { revalidate: 3600 } }
-  );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  if (!res.ok) {
+  if (!userResponse.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
   }
 
-  return res.json();
+  const res = await userResponse.json();
+  const { data: user }: { data: User } = res;
+
+  return user;
 }
 
 export default async function Home() {
@@ -74,7 +30,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen w-screen flex-col items-center justify-between">
       <Matrix />
-      {/* <TweetWrapper userData={user} /> */}
+      <TweetWrapper userData={user} />
     </main>
   );
 }
