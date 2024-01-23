af3747bf37adef49e56f04c6c403a26e37fba4a4
Jordan Molina <jordanmichaelmolina3@gmail.com>
2024-01-22 23:35:57 -0600

    clean up v2

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import withPlaiceholder from "@plaiceholder/next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -14,4 +12,4 @@ const nextConfig = {
   },
 };
 
-export default withPlaiceholder(nextConfig);
+export default nextConfig;
